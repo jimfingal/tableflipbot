f44d18410280d864af6ace0126f9f9910d66ed99
Jim Fingal <jim.fingal@gmail.com>
2014-12-31 14:39:38 -0800

    log event type

--- tableflipbot/followerflipper.py
+++ tableflipbot/followerflipper.py
@@ -8,13 +8,15 @@ class FollowFlipper(StreamListener):
     def on_event(self, status):
 
         try:
-            string_to_flip = ""
+            string_to_flip = None
             source_user = ""
 
             if status.event ==  "list_member_added":
+                logging.info("Got list member added event")
                 string_to_flip = status._json["target_object"]["name"]
                 source_user = status._json["source"]["screen_name"]
-            elif status.event ==   "follow":
+            elif status.event ==  "follow":
+                logging.info("Got Follow event event")
                 string_to_flip = status._json["source"]["screen_name"]
                 source_user = string_to_flip
 
@@ -27,5 +29,6 @@ class FollowFlipper(StreamListener):
                 status_out = ". @" + source_user + " ::\n" + flipped.decode('utf-8')
 
                 self.api.update_status(status_out)
+
         except Exception as e:
             logging.exception(e)
